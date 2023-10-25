f9e2060a2a78a32e05e52c7f269ae8697f8fc1af
Suhrud1511 <suhrudclicks123@gmail.com>
2023-10-25 15:52:23 +0530

    This is the third commit for lin regression pkl file

---
+++ app.py
@@ -0,0 +1,25 @@
+
+import json
+import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+## Load the model
+regmodel=pickle.load(open('linear_regression_model.pkl','rb'))
+scalar=pickle.load(open('scalar.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=regmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
