01f698532d3b6451117b5f016080e9aa9df70cf5
Suhrud1511 <suhrudclicks123@gmail.com>
2023-10-25 17:29:54 +0530

    s

--- app.py
+++ app.py
@@ -1,39 +1,42 @@
-
-import json
 import pickle
-
-from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
-import pandas as pd
+from flask import Flask, request, jsonify, render_template
 
-app=Flask(__name__)
-## Load the model
-regmodel=pickle.load(open('linear_regression_model.pkl','rb'))
-scalar=pickle.load(open('scalar.pkl','rb'))
+app = Flask(__name__)
+
+# Load the models and scalers for different algorithms
+linear_reg_model = pickle.load(open('linear_regression_model.pkl', 'rb'))
+xgboost_model = pickle.load(open('xgboost_model.pkl', 'rb'))
+linear_reg_scaler = pickle.load(open('scalar.pkl', 'rb'))
+xgboost_scaler = pickle.load(open('scalar.pkl', 'rb'))
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
-
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    try:
+        # Get user inputs from the form
+        data = [float(x) for x in request.form.values()]
+        input_data = np.array(data).reshape(1, -1)
 
+        # Check which model the user has selected
+        selected_model = request.form['model']
 
+        # Predict based on the selected model
+        if selected_model == 'linear':
+            input_data_scaled = linear_reg_scaler.transform(input_data)
+            prediction = linear_reg_model.predict(input_data_scaled)[0]
+        elif selected_model == 'xgboost':
+            input_data_scaled = xgboost_scaler.transform(input_data)
+            prediction = xgboost_model.predict(input_data_scaled)[0]
+        else:
+            return render_template("home.html", prediction_text="Invalid model selection")
 
-if __name__=="__main__":
+        return render_template("home.html", prediction_text=f"The House price prediction is {prediction:.2f}")
+    except Exception as e:
+        return render_template("home.html", prediction_text=f"Error: {str(e)}")
+
+if __name__ == '__main__':
     app.run(debug=True)
-    
