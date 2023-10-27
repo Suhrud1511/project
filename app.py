63d0d1783af1a5a7b00fe89918f52961ea0b254c
Suhrud1511 <suhrudclicks123@gmail.com>
2023-10-27 11:40:54 +0530

    s

--- app.py
+++ app.py
@@ -14,15 +14,15 @@ scalar=pickle.load(open('scalar.pkl','rb'))
 def home():
     return render_template('home.html')
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+# @app.route('/predict_api',methods=['POST'])
+# def predict_api():
+#     data=request.json['data']
+#     print(data)
+#     print(np.array(list(data.values())).reshape(1,-1))
+#     new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+#     output=regmodel.predict(new_data)
+#     print(output[0])
+#     return jsonify(output[0])
 
 @app.route('/predict',methods=['POST'])
 def predict():
